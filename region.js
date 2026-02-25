fa5cfbd51102174ea15c62a533d0800aa4a92d90
yowa185 <k76054266@gmail.com>
2026-02-25 19:28:29 +0900

    Add files via upload

--- region.js
+++ region.js
@@ -7,78 +7,78 @@ const regionData = {
             { name: "• 닷사이 [獺祭]", link: "detailedpage.html?brand=dassai" },
             { name: "• 고쿄 [五橋]", link: "detailedpage.html?brand=gokyou" }
         ],
-        position: { x: 390, y: 610 },
-        iconPos: { x: 355, y: 670 }
+        position: { x: 300, y: 500 },
+        iconPos: { x: 288, y: 537 }
     },
     "JP-18": { // 후쿠이
         name: "후쿠이",
         labelId: "fukui-label",
         sake: [{ name: "• 고쿠류 [黒龍]", link: "detailedpage.html?brand=kokuryu" }],
-        position: { x: 590, y: 530 },
-        iconPos: { x: 570, y: 576 }
+        position: { x: 470, y: 420 },
+        iconPos: { x: 455, y: 460 }
     },
     "JP-15": { // 니가타
         name: "니가타",
         labelId: "niigata-label",
         sake: [{ name: "• 쿠보타 [久보타]", link: "detailedpage.html?brand=kubota" }],
-        position: { x: 710, y: 440 },
-        iconPos: { x: 690, y: 485 }
+        position: { x: 570, y: 340 },
+        iconPos: { x: 553, y: 388 }
     },
     "JP-01": { // 홋카이도
         name: "홋카이도",
         labelId: "hokkaido-label",
         sake: [{ name: "• 오토코야마 [男山]", link: "detailedpage.html?brand=otokoyama" }],
-        position: { x: 870, y: 170 },
-         iconPos: { x: 860, y: 135 }
+        position: { x:350, y: 160 },
+         iconPos: { x: 690, y: 110 }
     },
     "JP-17": { // 이시카와
         name: "이시카와",
         labelId: "ishikawa-label",
         sake: [{ name: "• 기쿠히메 [菊姫]", link: "detailedpage.html?brand=kikuhime" }],
-        position: { x: 590, y: 500 },
-        iconPos: { x: 590, y: 530 }
+        position: { x: 480, y: 380 },
+        iconPos: { x: 473, y: 425 }
     },
     "JP-34": { // 히로시마
         name: "히로시마",
         labelId: "hiroshima-label",
         sake: [{ name: "• 가모츠루 [賀茂鶴]", link: "detailedpage.html?brand=kamotsuru" }],
-        position: { x: 440, y: 600 },
-        iconPos: { x: 410, y: 650 }
+        position: { x: 350, y: 480 },
+        iconPos: { x: 330, y: 520 }
     },
     "JP-20": { // 나가노
         name: "나가노",
         labelId: "nagano-label",
         sake: [{ name: "• 쿠로에몬 [九郎右衛門]", link: "detailedpage.html?brand=kyurouemon" }],
-        position: { x: 670, y: 520 },
-        iconPos: { x: 648, y: 560 }
+        position: { x: 540, y: 405 },
+        iconPos: { x: 520, y: 448 }
     },
     "JP-29": { // 나라
         name: "나라",
         labelId: "nara-label",
         sake: [{ name: "• 하루시카 [春鹿]", link: "detailedpage.html?brand=harusika" }],
-        position: { x: 560, y: 620 },
-        iconPos: { x: 552, y: 665 }
+        position: { x: 450, y: 500 },
+        iconPos: { x: 442, y: 533 }
     },
     "JP-03": { // 이와테
         name: "이와테",
         labelId: "iwate-label",
         sake: [{ name: "• 난부비진 [南部美人]", link: "detailedpage.html?brand=nambubijin" }],
-        position: { x: 820, y: 325 },
-        iconPos: { x: 798, y: 368 }
+        position: { x: 660, y: 245 },
+        iconPos: { x: 639, y: 295 }
     },
     "JP-06": { // 야마가타
         name: "야마가타",
         labelId: "yamagata-label",
         sake: [{ name: "• 데와자쿠라 [出羽桜]", link: "detailedpage.html?brand=dewazakura" }],
-        position: { x: 760, y: 390 },
-        iconPos: { x: 744, y: 435 }
+        position: { x: 610, y: 305 },
+        iconPos: { x: 595, y: 345 }
     },
     "JP-28": { // 효고
         name: "효고",
         labelId: "hyogo-label",
         sake: [{ name: "• 송죽매 [松竹梅]", link: "detailedpage.html?brand=takara" }],
-        position: { x: 520, y: 580 },
-        iconPos: { x: 504, y: 627 }
+        position: { x: 420, y: 460 },
+        iconPos: { x: 404, y: 503 }
 
     }
 };
